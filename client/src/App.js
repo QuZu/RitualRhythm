5bf5037936e36c405599547a35f1b72c38d5005a
Enis Mert Kuzu <emertkuzu35@gmail.com>
2024-11-22 16:34:53 +0100

    add react-router-dom for routing and implement basic navigation in App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,34 @@
-import './App.css';
+import React from 'react';
+import {Routes, Route, Link } from 'react-router-dom';
+import './styles/App.css';
+
+// Pages
+import Dashboard from './pages/Dashboard';
+import AddHabit from './pages/AddHabit';
+import DeleteHabit from './pages/DeleteHabit';
 
 function App() {
   return (
-    <div>
-      <h1>Welcome to Habit Tracker</h1>
-    </div>
+    <React.Suspense fallback={<div>Loading...</div>}>
+    <nav>
+          <ul style={{ display: 'flex', gap: '1rem', listStyle: 'none', padding: 0 }}>
+            <li>
+              <Link to="/">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="/add">Add</Link>
+            </li>
+            <li>
+              <Link to="/delete">Delete</Link>
+            </li>
+          </ul>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/add" element={<AddHabit />} />
+      <Route path="/delete" element={<DeleteHabit />} />
+    </Routes>
+    </React.Suspense>
   );
 }
 
