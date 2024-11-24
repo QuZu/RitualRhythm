ebfc9a34599210a361d2ad619c9c3cdf40e27852
Enis Mert Kuzu <emertkuzu35@gmail.com>
2024-11-24 18:04:16 +0100

    feat: enhance DeleteHabit component
    Implemented sorting habits by Opened date in the DeleteHabit component.
    Update styling for the DeleteHabit component.

--- client/src/pages/DeleteHabit.jsx
+++ client/src/pages/DeleteHabit.jsx
@@ -12,7 +12,12 @@ const DeleteHabit = () => {
     const fetchHabits = async () => {
       try {
         const response = await axios.get('http://localhost:5000/api/habits');
-        setHabits(response.data);
+        const sortedHabits=response.data.sort((a, b) => {
+          const dateA = new Date(a.openDate);
+          const dateB = new Date(b.openDate);
+          return dateB - dateA;
+        });
+        setHabits(sortedHabits);
       } catch (error) {
         console.error('Error fetching habits:', error);
         setErrorMessage('Failed to load habits. Please try again.');
@@ -39,7 +44,7 @@ const DeleteHabit = () => {
   return (
     <div className="delete-habit-container">
       <h1 className="delete-habit-title">Delete Habit</h1>
-      <p>Select a habit to delete from the list below:</p>
+      <p className='delete-habit-info'>Select a habit to delete from the list below:</p>
 
       {errorMessage && <p className="error-message">{errorMessage}</p>}
       {successMessage && <p className="success-message">{successMessage}</p>}
@@ -49,11 +54,12 @@ const DeleteHabit = () => {
       ) : (
         <ul className="delete-habit-list">
           {habits.map((habit) => (
-            <li key={habit.id} className="delete-habit-item">
+            <li key={habit.id} className={`delete-habit-item ${habit.completedDate ? 'completed' : 'uncompleted'}`}>
               <div>
                 <h3>{habit.name}</h3>
                 <h4><b>{"Habit Day: "}</b>{habit.day}</h4>
                 <p><b>{"Description: "}</b>{habit.description}</p>
+                <p><b>{"Open Date: "}</b>{new Date(habit.openDate).toLocaleDateString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit'})}</p>
               </div>
               <button
                 onClick={() => handleDeleteHabit(habit.id)}
