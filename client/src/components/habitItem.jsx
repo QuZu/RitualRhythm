79827f82987412a1b3e9643209d08911f6bd3a5e
Enis Mert Kuzu <emertkuzu35@gmail.com>
2024-11-24 17:49:40 +0100

    feat: enhance HabitItem and AddHabit components with improved time display and styling

--- client/src/components/habitItem.jsx
+++ client/src/components/habitItem.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import '../styles/HabitItem.css';
 
-const HabitItem = ({ habit, onComplete, isCompleted,completionTime }) => {
+const HabitItem = ({ habit, onComplete, isCompleted,completionTime,openTime }) => {
   const formattedTime = completionTime
     ? new Date(completionTime).toLocaleString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit',weekday: 'short' })
+    : '';
+  const formattedOpenTime = openTime
+    ? new Date(openTime).toLocaleString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit',weekday: 'short' })
     : '';
   return (
     <li className={`default-habit-item ${isCompleted ? 'completed' : 'uncompleted'}`}>
       <h3>{habit.name}</h3>
-      <p>{habit.description}</p>
-      {!isCompleted && (
-        <button onClick={onComplete} className="complete-button">Complete</button>
-      )}
-      {isCompleted && 
-      (
-          <p className="completion-time">Completed on {formattedTime}</p>
-      )}
+      <div className='default-habit-item-content'>
+        <p className='habit-description'>{habit.description}</p>
+        {!isCompleted && (
+          <button onClick={onComplete} className="complete-button">Complete</button>
+        )}
+        <p className="open-time"><b>Opened:</b> {formattedOpenTime}</p>
+        {isCompleted && 
+        (
+            <p className="completion-time"><b>Completed: </b>{formattedTime}</p>
+        )}
+      </div>
     </li>
   );
 };
--- client/src/pages/AddHabit.jsx
+++ client/src/pages/AddHabit.jsx
@@ -1,78 +1,13 @@
 import React, { useState } from 'react';
-import axios from 'axios';
+
 import '../styles/AddHabit.css';
+import HabitForm from '../components/habitForm';
 
 const AddHabit = () => {
-  const [name, setName] = useState('');
-  const [description, setDescription] = useState('');
-  const [day, setDay] = useState('Monday');
-  const [successMessage, setSuccessMessage] = useState('');
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const newHabit = { name, description, day };
-
-    try {
-      const response = await axios.post('http://localhost:5000/api/habits', newHabit);
-      if (response.status === 201) {
-        setSuccessMessage('Habit added successfully!');
-        setName('');
-        setDescription('');
-        setDay('Monday');
-      }
-    } catch (error) {
-      console.error('Error adding habit:', error);
-      setSuccessMessage('Failed to add habit. Try again.');
-    }
-  };
-
   return (
     <div className="add-habit-container">
       <h1 className="add-habit-title">Add Habit</h1>
-      <form onSubmit={handleSubmit} className="add-habit-form">
-        <input
-          type="text"
-          placeholder="Habit Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
-          className="add-habit-input"
-        />
-        <textarea
-          placeholder="Habit Description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          className="add-habit-textarea"
-        />
-        <select
-          value={day}
-          onChange={(e) => setDay(e.target.value)}
-          className="add-habit-select"
-        >
-          <option value="Monday">Monday</option>
-          <option value="Tuesday">Tuesday</option>
-          <option value="Wednesday">Wednesday</option>
-          <option value="Thursday">Thursday</option>
-          <option value="Friday">Friday</option>
-          <option value="Saturday">Saturday</option>
-          <option value="Sunday">Sunday</option>
-        </select>
-        <button type="submit" className="add-habit-button">
-          Add Habit
-        </button>
-      </form>
-      {successMessage && (
-        <p
-          className={
-            successMessage.includes('successfully')
-              ? 'add-habit-success'
-              : 'add-habit-error'
-          }
-        >
-          {successMessage}
-        </p>
-      )}
+      <HabitForm></HabitForm>
     </div>
   );
 };
